9eb80a64a141a8fa69f047fa1465bdb9eec0772e
kamothi <rhkddlf7911@naver.com>
2023-09-11 23:19:19 +0900

    fix: app.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,18 @@ import CreatePost from './Page/CreatePost';
 import './App.css';
 
 function App() {
-
+    const ContentContainer = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: center; /* 수평 가운데 정렬 */
+    align-items: center; /* 수직 가운데 정렬 */
+    padding: 10% 20%;
+`   ;
   return (
     <div style ={{display:"flex",height: "100%"}}>
     {/* 이 SideBar를 component로 나누고 react response 사용(useMediaQuery) */}
         <SideBar/>
-        <div style={{display:"flex",paddingLeft:"20%",height: "100%",width:"100%"}}>
+        <ContentContainer>
             <div style={{margin:"10px 180px"}}>
                 <Routes>
                     <Route path="/" element={<Post/>} />
@@ -21,7 +27,7 @@ function App() {
                     <Route path="/createPost" element={<CreatePost/>}/>
                 </Routes>
             </div>
-        </div>
+        </ContentContainer>
     </div>
   );
 }
